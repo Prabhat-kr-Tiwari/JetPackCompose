be9fcc7f4d4f0e3e8e598650982a0d049c3c2e88
Prabhat kumar tiwari <prabhat.mobilefirst@gmail.com>
2024-05-11 16:29:36 +0530

    states and remeber

--- app/src/main/java/com/prabhat/Introduction/MainActivity.kt
+++ app/src/main/java/com/prabhat/Introduction/MainActivity.kt
@@ -4,26 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.prabhat.Introduction.ui.theme.IntroductionTheme
@@ -35,124 +28,58 @@ class MainActivity : ComponentActivity() {
         setContent {
             IntroductionTheme {
 
-                firstUI()
+                Screen()
             }
         }
     }
 
     @Preview
     @Composable
-    fun firstUI() {
+    fun Screen() {
 
-
-        /*Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight(0.5f)
-                .background(MaterialTheme.colorScheme.primaryContainer )
-                .padding(
-
-                    vertical = 10.dp
-
-                *//*start = 40.dp, top = 40.dp*//*)
-
-            ,
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-
-            Button(onClick = { *//*TODO*//* }) {
-
-                Text(text = "Button 1")
-            }
-
-            Button(onClick = { *//*TODO*//* }) {
-                Text(text = "Button 2")
-            }
-        }*/
 
         Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight(0.5f)
+            modifier = Modifier.fillMaxSize(),
 
-                .background(MaterialTheme.colorScheme.secondary)
-
-                .padding(vertical = 40.dp)
-                .clip(RoundedCornerShape(20.dp))
-                .alpha(.8f)
-                .border(12.dp, Color.Green)
-                .padding(10.dp)
-                .border(12.dp, Color.Yellow)
-                .clickable { }
-                .verticalScroll(rememberScrollState())
-                .background(MaterialTheme.colorScheme.primaryContainer)
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
 
-            Button(
-                onClick = { },
-//                modifier = Modifier.offset(10.dp)
-                modifier = Modifier.padding(start = 20.dp)
-            ) {
-
-                Text(text = "Button 1")
-            }
-            //spacer
-            Spacer(modifier = Modifier.height(40.dp))
-
-            Button(onClick = { }) {
-                Text(text = "Button 2")
+            val textVisibilityState = remember {
+                mutableStateOf(true)
             }
 
-            Button(
-                onClick = { },
-//                modifier = Modifier.offset(10.dp)
-                modifier = Modifier.padding(start = 20.dp)
-            ) {
+            /*if (textVisibilityState.value) {
+                Text(text = "Text visibile")
 
-                Text(text = "Button 1")
-            }
+            }*/
 
-            Button(onClick = { }) {
-                Text(text = "Button 2")
-            }
-            Button(
-                onClick = { },
-//                modifier = Modifier.offset(10.dp)
-                modifier = Modifier.padding(start = 20.dp)
-            ) {
+            /*Text(
+                modifier = Modifier.alpha(if (textVisibilityState.value) 1f else 0.3f),
+                text = "Text visibile"
+            )*/
 
-                Text(text = "Button 1")
-            }
+            val text=if(textVisibilityState.value) "Visible Text " else "Note visible Text"
 
-            Button(onClick = { }) {
-                Text(text = "Button 2")
-            }
 
-            Button(
-                onClick = { },
-//                modifier = Modifier.offset(10.dp)
-                modifier = Modifier.padding(start = 20.dp)
-            ) {
+            Text(text = text, modifier = Modifier.alpha(
 
-                Text(text = "Button 1")
-            }
+                if(textVisibilityState.value) 1f else 0.2f
+            ))
 
-            Button(onClick = { }) {
-                Text(text = "Button 2")
-            }
-            Button(
-                onClick = { },
-//                modifier = Modifier.offset(10.dp)
-                modifier = Modifier.padding(start = 20.dp)
-            ) {
+            Spacer(modifier = Modifier.height(20.dp))
 
-                Text(text = "Button 1")
-            }
+            Button(onClick = {
 
-            Button(onClick = { }) {
-                Text(text = "Button 2")
+                textVisibilityState.value = !textVisibilityState.value
+
+            }) {
+
+                Text(text = "Change text  visibility")
+
             }
         }
+
     }
 
 }
