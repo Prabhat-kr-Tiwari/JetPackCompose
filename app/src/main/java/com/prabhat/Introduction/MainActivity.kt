80f362e3bfc567f06b2adda383ad2bd5dfbb3b0d
kandarp-petnotify <sonukbc6@gmail.com>
2024-10-08 17:23:13 +0530

    canvas

--- app/src/main/java/com/prabhat/Introduction/MainActivity.kt
+++ app/src/main/java/com/prabhat/Introduction/MainActivity.kt
@@ -4,30 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.animation.animateColor
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.rememberInfiniteTransition
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Canvas
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.unit.dp
 import com.prabhat.Introduction.ui.theme.IntroductionTheme
 
@@ -40,114 +31,62 @@ class MainActivity : ComponentActivity() {
         setContent {
             IntroductionTheme {
 
-
                 Column(
                     modifier = Modifier.fillMaxSize(),
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
 
-                    var isVisible by remember {
-
-                        mutableStateOf(false)
-                    }
-
-                    /* var size by remember {
-
-                         mutableStateOf(20.dp)
-                     }*/
-                    /*val size by animateDpAsState(
-                        targetValue = if (isVisible) 300.dp else 20.dp,
-                        label = "size",
-                        animationSpec = tween(
-                            delayMillis = 500,
-                            durationMillis = 5000,
-                            easing = FastOutSlowInEasing
-                        )
-                    )*/
-                    /* val color by animateColorAsState(
-                         targetValue = if (isVisible) Color.Blue else Color.Red,
-                         label = "color",
-                         animationSpec = tween(
-                             delayMillis = 500,
-                             durationMillis = 5000,
-                             easing = FastOutSlowInEasing
-                         )
-                     )*/
-
-                    //infinite animation
-                   /* val transition = rememberInfiniteTransition(label = "infinite animation")
-                    val color by transition.animateColor(
-                        initialValue = Color.Magenta,
-                        targetValue = Color.Yellow,
-                        animationSpec = infiniteRepeatable(tween(3000)),
-                        label = "infinite animation"
-                    )*/
-
-                    //change shape
-                    val corner by animateDpAsState(targetValue = if(isVisible) 300.dp else 20.dp,
-                        label = ""
-                    )
-
-                    Spacer(modifier = Modifier.height(59.dp))
-
-                    Button(onClick = {
-                        isVisible = !isVisible
-//                        size= 300.dp
-                    }
-
-                    ) {
-
-                        Text(text = "Click")
-
-                    }
-                    Spacer(modifier = Modifier.height(59.dp))
-
-
-                    Column(
-                        modifier = Modifier
-                            /*size(300.dp)*/
-
-                            .size(300.dp)
-//                            .background(MaterialTheme.colorScheme.primary)
-                             .clip(RoundedCornerShape(corner))
-                            .background(MaterialTheme.colorScheme.primary)
-                            .padding(16.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-
-                        /*if (isVisible) {
-
-
-                            Box(
-                                modifier = Modifier
-                                    .size(200.dp)
-                                    .background(Color.Black)
-                            ) {
-
-                            }
-                        }*/
-
-                        /*AnimatedVisibility(
-                            visible = isVisible,
-                            enter=slideInHorizontally()+fadeIn(),
-                            exit = slideOutHorizontally()+ fadeOut()
-                        ) {
-                            Box(
-                                modifier = Modifier
-                                    .size(200.dp)
-                                    .background(Color.Black)
-                            ) {
-
-                            }
-
-                        }*/
-
-                        //Animating the size
-
-                    }
+                    CanvasScreen()
                 }
 
             }
+        }
+    }
+
+
+}
+
+@Composable
+fun CanvasScreen() {
+
+    androidx.compose.foundation.Canvas(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(20.dp)
+            .height(500.dp)
+            .background(Color.Blue)
+
+    ) {
+
+
+        val width=size.width
+        val height=size.height
+
+        val recWidth=60.dp.toPx()
+        val recHeight=60.dp.toPx()
+        drawRect(
+            color = Color.Yellow
+            ,size= Size(recWidth,recHeight)
+            , topLeft = Offset(
+                x=(width-recWidth)/2,
+                y=(height-recHeight)/2
+            )
+        )
+
+        drawCircle(
+            brush = Brush.verticalGradient(listOf(Color.Green,Color.White))
+            , center = Offset(
+                x=width/2,
+                y=60.dp.toPx()+30.dp.toPx()
+            ),
+            radius = 60.dp.toPx()
+        )
+
+        val trianglePath= Path().apply{
+
+            moveTo(x=width/2,y=height-150.dp.toPx())
+
+
         }
     }
 
